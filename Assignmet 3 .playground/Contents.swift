5f332901d9e314e2f6b7be3fbd93a2425086b66b
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-09-05 06:53:54 -1000

    Completed I hope

---
+++ Assignmet 3 .playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// Assignment #3 Introduction to True and False
+
+var appState = false
+
+print(appState)
+//create 2 new var of type int
+
+var numberOne = 5
+var numberTwo = 7
+
+var comparison = numberOne<numberTwo
+var falseComparison = numberOne>numberTwo
+
+print (comparison)
+
+// two var equal
+
+var varOne = 8
+var varTwo = 8
+
+var newComparison = varOne==varTwo
+
+print(newComparison)
